6f615d41da6bbb3c185c3f9666944b96ded2a4e5
aolmo <aolmoher@uccs.edu>
2019-03-05 18:07:39 -0700

    Added loop to apply procedure in all images from a given folder

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
-import numpy as np
+import os
 import cv2
+import glob
+import scipy.misc
+import numpy as np
 import matplotlib.pyplot as plt
 
-__TEST_IMG = "data/dog.jpg"
+__TEST_IMG__ = "data/dog.jpg"
+__RESULTS_FOLDER__ = "results/"
+__IMGS_FOLDER__ = "data/"
 
 def display_img(rgb_img):
     plt.imshow(rgb_img)
@@ -11,28 +16,45 @@ def display_img(rgb_img):
     plt.show()
 
 
-def get_img():
-    bgr_img = cv2.imread(__TEST_IMG)
+def get_sample_img():
+    bgr_img = cv2.imread(__TEST_IMG__)
     # get bgr and switch to rgb
     b, g, r = cv2.split(bgr_img)
     rgb_img = cv2.merge([r, g, b])
     return rgb_img
 
+def apply_mask(mask, img):
+    cv2.rectangle(mask, (265, 17), (380, 137), 0, -1)
+    cv2.rectangle(mask, (0, 280), (100, 320), 0, -1)
+
+    # Apply the mask and return the result
+    return cv2.bitwise_and(img, mask)
+
+
+def get_img(img):
+    bgr_img = cv2.imread(img)
+    # get bgr and switch to rgb
+    b, g, r = cv2.split(bgr_img)
+    rgb_img = cv2.merge([r, g, b])
+    return rgb_img
 
 if __name__ == '__main__':
-    img = get_img()
-    # When performing the bitwise and we need to have 0s in the mask
-    # and the original RGB value for the other regions, thus in a
-    # Bitwise operation, we need whatever RGB value is and 2)11111111 = 10)255
-    mask = np.full_like(img, 255)
 
-    # Drawing of our masks
-    cv2.rectangle(mask, (265, 17), (380, 137), 0, -1)
-    cv2.rectangle(mask, (0,280), (100, 320), 0, -1)
+    for file in glob.glob(__IMGS_FOLDER__+"*.*"):
+        name, ext = os.path.splitext(file.split("/")[-1])
 
-    print(mask)
+        img = get_img(file)
+        # When performing the bitwise and we need to have 0s in the mask
+        # and the original RGB value for the other regions, thus in a
+        # Bitwise operation, we need whatever RGB value is and 2)11111111 = 10)255
+        mask = np.full_like(img, 255)
 
-    # Apply the mask and display the result
-    maskedImg = cv2.bitwise_and(img, mask)
-    display_img(maskedImg)
+        # Apply the mask and display image
+        masked_img = apply_mask(mask, img)
+
+        # Save image
+        scipy.misc.imsave(__RESULTS_FOLDER__+name+"_masked"+ext, masked_img)
+
+        # TODO: Save mask too
+        display_img(masked_img)
 
