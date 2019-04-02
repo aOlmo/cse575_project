4d2c8b7198214e4cc1d7c33d64d33257260cc25f
Matt Davis <mwdavis3@asu.edu>
2019-04-01 17:42:50 -0700

    Updated script.py for circle patches

--- script.py
+++ script.py
@@ -10,11 +10,13 @@ __TEST_IMG__ = "data/dog.jpg"
 __RESULTS_FOLDER__ = "results/"
 __IMGS_FOLDER__ = "data/"
 
+
 def display_img(rgb_img):
     plt.imshow(rgb_img)
     # to hide tick values on X and Y axis
     plt.xticks([]), plt.yticks([])
     plt.show()
+
 
 def get_sample_img():
     bgr_img = cv2.imread(__TEST_IMG__)
@@ -23,13 +25,23 @@ def get_sample_img():
     rgb_img = cv2.merge([r, g, b])
     return rgb_img
 
+
 # TODO: Create more masks
-def get_mask(mask):
+def get_rectangle_mask(mask):
     val = 255
     cv2.rectangle(mask, (221, 100), (130, 137), (val, val, val), -1)
     cv2.rectangle(mask, (0, 210), (100, 120), (val, val, val), -1)
 
     return mask
+
+
+def get_circle_mask(mask):
+    val = 255  # white = rgb(255, 255, 255)
+    cv2.circle(mask, (70, 200), 30, (val, val, val), -1)  # circle(img, point center, int radius, color rgb, thickness)
+    cv2.circle(mask, (200, 60), 50, (val, val, val), -1)
+
+    return mask
+
 
 def apply_blur(img, intensity=35):
     blurred_image = cv2.blur(img, (intensity, intensity))
@@ -44,10 +56,9 @@ def get_img(img):
     return rgb_img
 
 
-
 if __name__ == '__main__':
 
-    for i, file in enumerate(glob.glob(__IMGS_FOLDER__+"*.*")):
+    for i, file in enumerate(glob.glob(__IMGS_FOLDER__ + "*.*")):
         name, ext = os.path.splitext(file.split("/")[-1])
         img = get_img(file)
         img = cv2.resize(img, (256, 256))
@@ -55,9 +66,10 @@ if __name__ == '__main__':
         mask_zeros = np.zeros_like(img)
         # blurred_img = apply_blur(img)
 
-        white_img = np.full_like(img, 160)
+        white_img = np.full_like(img, 255)
 
-        mask_zeros = get_mask(mask_zeros)
+        # mask_zeros = get_rectangle_mask(mask_zeros)
+        mask_zeros = get_circle_mask(mask_zeros)
         img_with_blurs = np.where(mask_zeros == np.array([255, 255, 255]), white_img, img)
 
         imgs_side_2_side = np.hstack((img, img_with_blurs))
