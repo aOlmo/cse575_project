1d7d134b4e92edc1649c4dc2150f74001d8cee0f
aolmo <aolmoher@uccs.edu>
2019-04-18 09:16:27 -0700

    Added tqdm and order when getting images

--- similarity_measures.py
+++ similarity_measures.py
@@ -2,6 +2,7 @@ import os
 import sys
 import cv2
 import glob
+from tqdm import tqdm
 import imageio
 import numpy as np
 import argparse
@@ -13,38 +14,45 @@ from skimage.measure import compare_mse as mse
 
 
 __TEST_FOLDER__ = "results/test/"
+MAX_IMAGES = 512
 
 def display_img(rgb_img):
     plt.imshow(rgb_img)
     plt.xticks([]), plt.yticks([])
     plt.show()
 
-def get_avg_metric(images, metric, split, save_dir_root):
+def print_metrics(ssim_sum, psnr_sum, i):
+    print("[+]: Iteration {}\nAvg SSIM: {}, Avg PSNR: {}".format(i, round(ssim_sum/i, 3), round(psnr_sum/i, 3)))
+
+
+def get_avg_metrics(images, split, save_dir_root):
     height, width = images.shape[1], images.shape[2]
 
     width_cutoff = width // 2
     s1 = images[..., :width_cutoff, :]
     s2 = images[..., width_cutoff:, :]
 
-    sum = 0
     i = 0
-    for left, right in zip(s1, s2):
+    ssim_sum = 0
+    psnr_sum = 0
+    for left, right in tqdm(zip(s1, s2), total=MAX_IMAGES):
+        i += 1
+
         if split:
             imageio.imsave(save_dir_root+"/sharp/" + str(i) + ".png", left)
             imageio.imsave(save_dir_root+"/blur/" + str(i) + ".png", right)
 
-        if (metric == "ssim"):
-            sum += ssim(left, right, data_range=right.max() - right.min(), multichannel=True)
-        elif (metric == "psnr"):
-            sum += psnr(left, right)
-        elif (metric == "mse"):
-            sum += mse(left, right)
-        else:
-            print("Metric not recognized")
-            exit()
-        i += 1
+        ssim_sum += ssim(left, right, data_range=right.max() - right.min(), multichannel=True)
+        psnr_sum += psnr(left, right)
 
-    return sum / images.shape[0]
+        if (i % 50 == 0) and i != 0:
+            print("[+]: Iteration {}".format(i))
+            print_metrics(ssim_sum, psnr_sum, i)
+
+        if i == MAX_IMAGES:
+            break
+
+    return ssim_sum / i, psnr_sum / i
 
 def make_directory(path):
     try:
@@ -60,7 +68,10 @@ if __name__ == '__main__':
     parser.add_argument("--folder", help="Folder where the images are")
     parser.add_argument("--split_sharp_blur", type=bool, help="Splits the images into two new folders")
 
+    parser.set_defaults(split_sharp_blur=True)
+
     args = parser.parse_args()
+
     split = args.split_sharp_blur
     imgs_folder = args.folder
 
@@ -69,20 +80,19 @@ if __name__ == '__main__':
     else:
         root_name = args.folder.split("/")[-2]
 
+    save_dir_root = ""
     if split:
         save_dir_root = "results/"+root_name+"/"
         make_directory(save_dir_root+"/sharp")
         make_directory(save_dir_root+"/blur")
 
-    images = np.array([cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in glob.glob(imgs_folder+"*.png")])
+    images = np.array([cv2.cvtColor(cv2.imread(file), cv2.COLOR_BGR2RGB) for file in sorted(glob.glob(imgs_folder+"*.png"))])
 
-    ssim_avg = round(get_avg_metric(images, "ssim", split, save_dir_root), 3)
-    psnr_avg = round(get_avg_metric(images, "psnr", split, save_dir_root), 3)
-    mse_avg = round(get_avg_metric(images, "mse", split, save_dir_root), 3)
+    ssim_avg, psnr_avg = get_avg_metrics(images, split, save_dir_root)
 
     print("\nFolder: {} | # of imgs: {}\n".format(imgs_folder, images.shape[0]))
     print("======== Averages ======== ")
-    print("SSIM: {} \nPSNR: {} \nMSE: {}".format(ssim_avg, psnr_avg, mse_avg))
+    print("SSIM: {} \nPSNR: {}".format(ssim_avg, psnr_avg))
     print("========================== ")
 
 
